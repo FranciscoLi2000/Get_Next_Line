9bb15e4f20a66cc18f35391a29894bc194860521
FranciscoLi2000 <francisco2000li@icloud.com>
2025-02-18 19:41:54 +0000

    main to test

--- main.c
+++ main.c
@@ -6,12 +6,21 @@ int main(void)
 {
 	int fd = open("test.txt", O_RDONLY);
 	char *line;
+    int line_number = 0;
+
+    if (fd == -1)
+    {
+        perror("Error opening file");
+        return (1);
+    }
 
 	while ((line = get_next_line(fd)) != NULL)
 	{
-		printf("%s", line);
+		printf("Line %d: %s", line_number + 1, line); // +1 para numeración humana
 		free(line);
+        line_number++;
 	}
+    printf("\nTotal lines: %d\n", line_number);
 	close(fd);
 	return (0);
 }
