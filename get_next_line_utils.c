d82ddf7ac4dc6887a93ab20522f6ee72e881fa64
FranciscoLi2000 <francisco2000li@icloud.com>
2025-02-18 19:35:33 +0000

    Obligatorio

---
+++ get_next_line_utils.c
@@ -0,0 +1,49 @@
+#include "get_next_line.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != (char)c)
+	{
+		if (*s == '\0')
+			return (NULL);
+		s++;
+	}
+	return ((char *)s);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		s1_len;
+	int		s2_len;
+	int		i;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	str = malloc((s1_len + s2_len + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (i < s1_len + s2_len)
+	{
+		if (i < s1_len)
+			str[i] = s1[i];
+		else
+			str[i] = s2[i - s1_len];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+int	ft_strlen(const char *str)
+{
+	int	len;
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+	return (len);
+}
