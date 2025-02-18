cc5455c9b16b015b209c8a65c2f2d63c0afb7a3c
FranciscoLi2000 <francisco2000li@icloud.com>
2025-02-18 19:52:59 +0000

    main to test

--- main.c
+++ main.c
@@ -2,25 +2,30 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
-	int fd = open("test.txt", O_RDONLY);
+	int fd;
 	char *line;
-    int line_number = 0;
+    int line_number;
 
-    if (fd == -1)
+    if (argc != 2)
     {
-        perror("Error opening file");
+        printf("Usage: %s <file name>\n", argv[0]);
         return (1);
     }
-
+    fd = open(argv[1], O_RDONLY);
+    if (fd < 0)
+    {
+        printf("Error abriendo el archivo: %s\n", argv[1]);
+        return (1);
+    }
+    line_number = 1;
 	while ((line = get_next_line(fd)) != NULL)
 	{
-		printf("Line %d: %s", line_number + 1, line); // +1 para numeración humana
+		printf("[Line %d] %s", line_number, line);
 		free(line);
         line_number++;
 	}
-    printf("\nTotal lines: %d\n", line_number);
 	close(fd);
 	return (0);
 }
