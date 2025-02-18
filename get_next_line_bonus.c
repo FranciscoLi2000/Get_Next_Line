918b13fb9a1288b438a6a5bcd84c3c75bd370dd3
FranciscoLi2000 <francisco2000li@icloud.com>
2025-02-18 23:39:48 +0000

    bonus

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -51,6 +51,81 @@ static void cleanup_node(t_list **head, int fd)
 	}
 }
 
+static char	*read_buffer(int fd, char *buffer)
+{
+	char	*tmp;
+	int		bytes_read;
+
+	tmp = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!tmp)
+		return (NULL);
+	bytes_read = 1;
+	while (!ft_strchr(buffer, '\n') && bytes_read > 0)
+	{
+		bytes_read = read(fd, tmp, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(tmp);
+			return (NULL);
+		}
+		tmp[bytes_read] = '\0';
+		buffer = ft_strjoin(buffer, tmp);
+	}
+	free(tmp);
+	return (buffer);
+}
+
+static char	*extract_line(char *buffer)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	if (!buffer[i])
+		return (NULL);
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	line = malloc((i + 2) * sizeof(char));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+	{
+		line[i] = buffer[i];
+		i++;
+	}
+	if (buffer[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
+
+static char	*update_buffer(char *buffer)
+{
+	char	*new_buffer;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (!buffer[i])
+	{
+		free(buffer);
+		return (NULL);
+	}
+	new_buffer = malloc((ft_strlen(buffer) - i + 1) * sizeof(char));
+	if (!new_buffer)
+		return (NULL);
+	i++;
+	j = 0;
+	while (buffer[i])
+		new_buffer[j++] = buffer[i++];
+	new_buffer[j] = '\0';
+	free(buffer);
+	return (new_buffer);
+}
+
 char	*get_next_line_bonus(int fd)
 {
 	static t_list	*head;
---
+++ main_bonus.c
@@ -0,0 +1,68 @@
+#include "get_next_line_bonus.h"
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    int *fds;
+    int num_files;
+    char *line;
+    int all_done;
+    int i;
+
+    if (argc < 2)
+    {
+        printf("Introduce texto (Ctrl+D para terminar):\n");// Leer de stdin si no hay argumentos
+        while ((line = get_next_line_bonus(STDIN_FILENO)))
+        {
+            printf("[stdin] > %s", line);
+            free(line);
+        }
+        return 0;
+    }
+
+    /* Abrir todos los archivos */
+    num_files = argc - 1;
+    fds = malloc(num_files * sizeof(int));
+    if (!fds)
+        return 1;
+
+    for (i = 0; i < num_files; i++)
+    {
+        fds[i] = open(argv[i + 1], O_RDONLY);
+        if (fds[i] == -1)
+        {
+            perror("Error abriendo archivo");
+            // Cerrar los ya abiertos
+            for (int j = 0; j < i; j++)
+                close(fds[j]);
+            free(fds);
+            return 1;
+        }
+    }
+
+    // Leer alternadamente de todos los archivos
+    printf("Leyendo alternadamente de %d archivos:\n", num_files);
+    do {
+        all_done = 1;
+        
+        for (i = 0; i < num_files; i++) {
+            if (fds[i] == -1) continue;
+            
+            line = get_next_line_bonus(fds[i]);
+            if (line) {
+                printf("[%s] > %s", argv[i + 1], line);
+                free(line);
+                all_done = 0;
+            } else {
+                close(fds[i]);
+                fds[i] = -1;
+            }
+        }
+    } while (!all_done);
+
+    free(fds);
+    return 0;
+}
